be0d2b0116ee39c096d5d9a95b4b5f2edcf219aa
tosif91 <61594032+tosif91@users.noreply.github.com>
2021-11-13 17:22:55 +0530

    added appbar and bottombar

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:talkrr/component/custom_appbar.dart';
+import 'package:talkrr/component/custom_bottom_bar.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      appBar: PreferredSize(
+          child: CustomAppBar(), preferredSize: Size.fromHeight(43)),
+      bottomNavigationBar: CustomerBottomBar(),
+    );
   }
 }
---
+++ lib/utils/common/const.dart
@@ -0,0 +1,2 @@
+const String profileURL =
+    "https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg";
